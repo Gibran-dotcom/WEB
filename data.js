792674b09b8704ce5ea73d8f9c96e40414ca9988
Gibran-dotcom <usuppbran12@gmail.com>
2026-04-24 10:13:42 +0700

    PELAJARAN 1

---
+++ data.js
@@ -0,0 +1,12 @@
+const databaseProyek = {
+    "sistem_pos-xyz": {
+        judul: "PT PERAIRAN INDONESIA",
+        keahlian: ['music,gitaris'],
+        gambar: "https://f4.bcbits.com/img/0043752070_10.jpg",
+        latar: "music band",
+        tantangan: "",
+        pelajaran: "",
+        link: ""
+        
+    },
+};
